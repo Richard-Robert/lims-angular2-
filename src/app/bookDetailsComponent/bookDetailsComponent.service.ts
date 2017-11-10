64e41e9c1db0640aa7830a7f96184038090c37f3
Richard Robert <M1037765@mindtree.com>
2017-11-10 18:36:22 +0530

    not empty message3

--- src/app/bookDetailsComponent/bookDetailsComponent.service.ts
+++ src/app/bookDetailsComponent/bookDetailsComponent.service.ts
@@ -4,31 +4,51 @@ import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
 
 import { Book } from './../bookClass';
 @Injectable()
 export class BookDetailsService {
-books: any;
+books: any = {
+  bookList: []
+}
 bookIndex: number;
   constructor(private http: HttpClient) { }
 
   getBookDetails(isbn) {
-    return this.http.get('./../../assets/data.json')
-      .map((response: Response) => {
-        this.books = response;
-          for (const book of this.books.bookList) {
-            if (book.ISBN === isbn) {
-              this.bookIndex = this.books.bookList.indexOf(book);
-              return response = book;
-            }
-            }
-        });
+
+    this.books = JSON.parse(sessionStorage.getItem('booksData'));
+    if(this.books === null){
+        return this.http.get('./../../assets/data.json')
+            .map((response: Response) => {
+                this.books = response;
+                    for (const book of this.books.bookList) {
+                        if (book.ISBN === isbn) {
+                          this.bookIndex = this.books.bookList.indexOf(book);
+                          sessionStorage.setItem('booksData', JSON.stringify(this.books));
+                          // sessionStorage.setItem('bookDetailsIndex', JSON.stringify(this.bookIndex));
+                          return response = book;
+                        }
+                    }
+              });
+      }
+      else
+      {
+        for (const book of this.books.bookList) {
+                      if (book.ISBN === isbn) {
+                        this.bookIndex = this.books.bookList.indexOf(book);
+                        sessionStorage.setItem('booksData', JSON.stringify(this.books));
+                        // sessionStorage.setItem('bookDetailsIndex', JSON.stringify(this.bookIndex));
+                        return Observable.of(book);
+                      }
+                  }
+      }
   }
 
   insertReview(value) {
     this.books.bookList[this.bookIndex].comments.push(value);
-
-    let newReview= JSON.stringify(this.books);
-    this.http.post('./../../assets/data.json', newReview);
+    sessionStorage.setItem('booksData', JSON.stringify(this.books));
+    // let newReview= JSON.stringify(this.books);
+    // this.http.post('./../../assets/data.json', newReview);
   }
 }
